59021042d836ca32fbe14392e852461ca5c6ae11
Akib Ahmed <akibahmed7360@gmail.com>
2022-06-18 22:54:09 +0600

    Started working on react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.component";
 
-
+const HatsPAge = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-     <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/hats" element={<HatsPAge />} />
+      </Routes>
     </div>
   );
 }
