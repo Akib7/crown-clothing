ff2a41b6e4bfa61f0ac309f6fd2e1f0afd550d45
Akib Ahmed <akibahmed7360@gmail.com>
2022-06-25 16:33:27 +0600

    worked with firebase auth for saving user info in the app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from "./components/header/header.component";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInSignUpPage from "./pages/sign-in-and-sing-up/sign-in-and-sing-up.component";
+import { auth } from "./firebase/firebase.utils";
+
 // const HatsPAge = () => (
 //   <div>
 //     <h1>HATS PAGE</h1>
@@ -49,18 +51,42 @@ import SignInSignUpPage from "./pages/sign-in-and-sing-up/sign-in-and-sing-up.co
 //   );
 // };
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        {/* <Route path="/hats" element={<HatsPAge />} /> */}
-        <Route path="shop" element={<ShopPage />} />
-        <Route path="signin" element={<SignInSignUpPage />} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({
+        currentUser: user,
+      });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          {/* <Route path="/hats" element={<HatsPAge />} /> */}
+          <Route path="shop" element={<ShopPage />} />
+          <Route path="signin" element={<SignInSignUpPage />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
