b510d767ed8596f07656f7d09761a191135403a0
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-03 12:53:57 +0600

    started working with the cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 import { connect } from "react-redux";
 
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 import { ReactComponent as Logo } from "../../assests/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
@@ -31,6 +33,7 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
