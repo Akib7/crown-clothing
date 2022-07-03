a630a4ec7a7abcd3e226d8e03b61896914faa18d
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-04 00:26:07 +0600

    fixed the errors with cartItems, and item quantity

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,9 +3,7 @@ export const addToCart = (cartItems, itemToAdd) => {
 
   if (existingItems) {
     return cartItems.map((item) =>
-      item.id === itemToAdd.id
-        ? { ...cartItems, quantity: cartItems.quantity + 1 }
-        : cartItems
+      item.id === itemToAdd.id ? { ...item, quantity: item.quantity + 1 } : item
     );
   }
   return [...cartItems, { ...itemToAdd, quantity: 1 }];
