98b4c6e0447ffc80884f42efdaac20a7003a892b
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-07 23:14:42 +0600

    worked on the checkout page and rendered the selected items' name

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -18,11 +18,18 @@ export const selectCartHidden = createSelector(
 );
 
 export const selectCartItemsCount = createSelector(
-  [selectCartItems, selectCartHidden],
+  [selectCartItems],
   (cartItems) =>
     cartItems.reduce(
       (accumalatedQuantity, cartItem) =>
         accumalatedQuantity + cartItem.quantity,
       0
     )
+);
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumalatedQuantity, cartItem) =>
+      accumalatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
 );
